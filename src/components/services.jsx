2de164bec1bd92bfd07f9c434b6b6e13d86ce40f
Kayla Matthews <kdmatthews77@gmail.com>
2024-07-31 16:24:55 -0400

    Update services.jsx

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import Header from './header'
 
 function Services() {
   return (
     <div>
-        <div>lorem100</div>
+        <Header />
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro alias facilis, nam, animi qui, repellat ipsum earum iste at obcaecati consequuntur dolore! Sit iste nesciunt hic impedit blanditiis alias atque.</div>
         <br />
-        <div>lorem100</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro alias facilis, nam, animi qui, repellat ipsum earum iste at obcaecati consequuntur dolore! Sit iste nesciunt hic impedit blanditiis alias atque.</div>
         <br />
-        <div>lorem100</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro alias facilis, nam, animi qui, repellat ipsum earum iste at obcaecati consequuntur dolore! Sit iste nesciunt hic impedit blanditiis alias atque.</div>
         <br />
-        <div>lorem100</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur pariatur iure sed dignissimos a officia quidem nulla, vero neque quos deserunt quo magnam ullam optio architecto quasi nostrum voluptatem ipsa iusto. Incidunt esse tempore voluptates obcaecati fuga, repellendus dolores inventore error unde atque excepturi veniam harum provident dolore voluptatibus reprehenderit, impedit iure maxime amet qui consectetur ad. Molestias possimus vero mollitia ipsum cupiditate repudiandae rerum et. A sapiente sequi nesciunt, laudantium, cupiditate non esse nemo cumque, iusto quis autem? Nobis animi vitae voluptate quisquam ab sapiente molestiae obcaecati beatae eaque, id itaque, a culpa, aperiam veritatis adipisci ipsum ad soluta.</div>
         <br />
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur pariatur iure sed dignissimos a officia quidem nulla, vero neque quos deserunt quo magnam ullam optio architecto quasi nostrum voluptatem ipsa iusto. Incidunt esse tempore voluptates obcaecati fuga, repellendus dolores inventore error unde atque excepturi veniam harum provident dolore voluptatibus reprehenderit, impedit iure maxime amet qui consectetur ad. Molestias possimus vero mollitia ipsum cupiditate repudiandae rerum et. A sapiente sequi nesciunt, laudantium, cupiditate non esse nemo cumque, iusto quis autem? Nobis animi vitae voluptate quisquam ab sapiente molestiae obcaecati beatae eaque, id itaque, a culpa, aperiam veritatis adipisci ipsum ad soluta.</div>
+        <br />
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur pariatur iure sed dignissimos a officia quidem nulla, vero neque quos deserunt quo magnam ullam optio architecto quasi nostrum voluptatem ipsa iusto. Incidunt esse tempore voluptates obcaecati fuga, repellendus dolores inventore error unde atque excepturi veniam harum provident dolore voluptatibus reprehenderit, impedit iure maxime amet qui consectetur ad. Molestias possimus vero mollitia ipsum cupiditate repudiandae rerum et. A sapiente sequi nesciunt, laudantium, cupiditate non esse nemo cumque, iusto quis autem? Nobis animi vitae voluptate quisquam ab sapiente molestiae obcaecati beatae eaque, id itaque, a culpa, aperiam veritatis adipisci ipsum ad soluta.</div>
     </div>
   )
 }
