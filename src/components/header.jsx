f2d3732d8d43823a35fcacd0146cfd2e1da8dab4
Kayla Matthews <kdmatthews77@gmail.com>
2024-08-02 23:13:29 -0400

    styling updates

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,13 @@
 import './header.css'
+import Navbar from './navbar';
+
 
 function Header() {
     return (
       <>
+      <Navbar></Navbar>
         <div className="bg">
-         
+          <Navbar></Navbar>
         </div>
 
       </>
