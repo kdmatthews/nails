58f5606cfca966fb76db80edb709b563c9e13471
Kayla Matthews <kdmatthews77@gmail.com>
2024-08-06 16:32:43 -0400

    Clean Up

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Services from './services'
-import GalleryScroll from './galleryscroll'
 import About from './about'
 import Contact from './contact'
+
 
 
 function Home() {
   return (
     <div>
-  
       <About />
       <Services />
       <Contact />
+
 </div>
 
   )
